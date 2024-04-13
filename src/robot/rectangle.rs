b623dc63509d151fc43ae2c513abb8bf1f5823a5
Mihail Kuzmin <testic1996@mail.ru>
2024-04-13 15:27:44 +0500

    Moving base

--- src/robot/rectangle.rs
+++ src/robot/rectangle.rs
@@ -4,28 +4,26 @@ use egui_plot::{Line, PlotPoints};
 pub struct Rectangle {
     pub lenght: f64,
     pub width: f64,
-    pub start_pos: Pos2,
-    pub end_pos: Pos2
+    pub center: Pos2
 }
 impl Rectangle {
     pub const fn new() -> Rectangle {
         Rectangle{
             lenght: 2.0,
             width: 0.5,
-            start_pos: Pos2{x: -1.0, y: -0.5},
-            end_pos: Pos2{x: 1.0, y: -0.5}
+            center: Pos2{x: 0.0, y: -0.5}
         }
     }
     pub fn make_line(&self) -> Line {
         Line::new(self.make_points())
-        .fill((self.start_pos.y - self.start_pos.y - self.width as f32) as f32)
+        .fill((self.center.y - self.center.y - self.width as f32) as f32)
         .name("base")
     }
     fn make_points(&self) -> PlotPoints {
-        vec![(self.start_pos.x as f64, self.start_pos.y as f64).into(),
-        (self.start_pos.x as f64 + self.lenght as f64,self.start_pos.y as f64).into(),
-        (self.start_pos.x as f64 + self.lenght as f64,self.start_pos.y as f64 - self.width as f64).into(),
-        (self.start_pos.x as f64,self.start_pos.y as f64 - self.width as f64).into(),
-        (self.start_pos.x as f64, self.start_pos.y as f64).into(),].into()
+        vec![(self.center.x as f64 - self.lenght as f64, self.center.y as f64).into(),
+        (self.center.x as f64 + self.lenght as f64,self.center.y as f64).into(),
+        (self.center.x as f64 + self.lenght as f64,self.center.y as f64 - self.width as f64).into(),
+        (self.center.x as f64 - self.lenght as f64,self.center.y as f64 - self.width as f64).into(),
+        (self.center.x as f64 - self.lenght as f64, self.center.y as f64).into(),].into()
     }
 }
